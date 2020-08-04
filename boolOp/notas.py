8aba8e0589e222efacb901b45f1a1724e8ce85dc
chus <jesusmacevedoarias@gmail.com>
2020-08-04 10:06:23 -0500

    Ejercicio Notas_estudiante | Booleanos y Operaciones

---
+++ boolOp/notas.py
@@ -0,0 +1,23 @@
+"""
+    Muy deficiente [0, 3)
+    Deficiente [3, 3.5)
+    Aceptable [3.5, 4)
+    Bueno [4,4.5)
+    Excelente [4.5, 5]
+"""
+nota = float(input("Ingrese la nota: "))
+
+#nota => [0,5]
+valida = nota>=0 and nota<=5
+muy_deficiente = nota>=0 and nota<3
+deficiente = nota>=3 and nota<3.5
+aceptable = nota>=3.5 and nota<4
+buena = nota>=4 and nota<4.5
+excelente = nota>=4.5 and nota<=5
+
+print(f"Nota valida: {valida}")
+print(f"Nota muy deficiente: {muy_deficiente}")
+print(f"Nota deficiente: {deficiente}")
+print(f"Nota aceptable: {aceptable}")
+print(f"Nota buena: {buena}")
+print(f"Nota excelente: {excelente}")
