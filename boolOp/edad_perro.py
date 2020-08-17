548b4030ae9dcf4a57970320b4875f483d110a6a
chus <jesusmacevedoarias@gmail.com>
2020-08-17 08:31:07 -0500

    Ejercicio Edad_Perro | Booleanos y Operaciones

---
+++ boolOp/edad_perro.py
@@ -0,0 +1,19 @@
+edad_humano = int(input("Edad del humano: "))
+
+#1=> 5 perro
+#2=> 10.5 perro
+#3=> 4.5 perro
+edad_perro = 0
+if edad_humano>=0:
+    if edad_humano==1:
+        edad_perro = 5
+    
+    if edad_humano==2:
+        edad_perro = 10.5
+    
+    if edad_humano>2:
+        edad_perro = ((edad_humano-2)*4.5)+10.5
+
+    print(f"La edad del perro es: {edad_perro}.")
+else:
+    print("Los datos no son validos.")
