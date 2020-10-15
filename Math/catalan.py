dfefc83fd2dd683cac448c2a23816837652e644d
William <williamyesid.10@gmail.com>
2020-10-14 23:45:31 -0500

    added catalan numbers

---
+++ Math/catalan.py
@@ -0,0 +1,11 @@
+#Guarda en el array Catalan Numbers los numeros de Catalan hasta MAX.
+
+MAX = 30;
+
+catalan = [0 for i in range(MAX+1)]    
+
+def catalanNumbers():
+	catalan[0] = 1;
+	for i in range(1, MAX+1):
+	    catalan[i] = catalan[i-1]*((float)(2*((2 * i)- 1))/(i + 1))
+	
