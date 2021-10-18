c8910be8332630b1f8982a4584aa254d9035086e
JoimarM <joimarfabianms@ufps.edu.co>
2021-10-18 15:24:59 -0500

    Agrego clase Docente junto con dos metodos

---
+++ POO/Docente.py
@@ -0,0 +1,13 @@
+class Persona:
+
+    def __init__(self,codigo, salario, area):
+        self.codigo = codigo
+        self.salario = salario
+        self.area = area
+
+    def retornar_codigo(self):
+        return self.codigo 
+    
+    def mostrar_salario(self):
+        self.salario = self.salario
+
